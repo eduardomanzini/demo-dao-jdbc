81a8b9a3911dfbfe804131049e5e17069a09cef9
eduardomanzini <eduardo_manzini@hotmail.com>
2022-12-21 11:08:02 -0300

    SellerDaoJDBC and DaoFactory

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -1,5 +1,18 @@
 package model.dao;
 
+import java.util.List;
+
+import model.entities.Department;
+import model.entities.Seller;
+
 public interface SellerDao {
 
+
+	void insert(Seller obj);
+	void udpate(Seller obj);
+	void deleteById(Integer id);
+	Department findByid(Integer id);
+	List<Seller> findAll();
+	
+	
 }
