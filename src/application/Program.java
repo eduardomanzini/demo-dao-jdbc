f13a8902039bd9aeb7fcb590d99768f6ade3cbbc
eduardomanzini <eduardo_manzini@hotmail.com>
2022-12-22 15:13:30 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,9 +2,12 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
+import db.DB;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.dao.impl.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,7 +15,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
@@ -46,6 +49,12 @@ public class Program {
 		seller.setName("Martha Waine");
 		sellerDao.udpate(seller);
 		System.out.println("Udpate completed");
+		
+		System.out.println("\n=== TEST 6 :seller delete ====");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed");
 	}
 
 }
